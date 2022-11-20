6e36b3a1c62a5fc519b5af7dd348d9f42b82425a
Kenta Kubo <kkk669@users.noreply.github.com>
2022-11-21 01:48:30 +0900

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,75 @@
+import ARKit
+import CoreMotion
+import SceneKit
+import SwiftUI
+
+struct ContentView {
+    @State private var manager = CMHeadphoneMotionManager()
+    @State private var motion: CMDeviceMotion?
+    @State private var attitudeOffset: CMAttitude?
+    @State private var scene = SCNScene()
+    @State private var faceNode = SCNNode()
+    @State private var cameraNode = SCNNode()
+
+    private func startUpdatingMotion() {
+        guard manager.isDeviceMotionAvailable else { return }
+        manager.startDeviceMotionUpdates(to: .main) { motion, error in
+            if let error { fatalError("\(error.localizedDescription)") }
+            guard let motion else { fatalError("no value") }
+            self.motion = motion
+            if attitudeOffset == nil {
+                attitudeOffset = motion.attitude
+            }
+
+            updateFace(attitude: motion.attitude)
+        }
+    }
+
+    private func updateFace(attitude: CMAttitude) {
+        guard let attitudeOffset else { fatalError() }
+        faceNode.eulerAngles.x = -Float(attitude.pitch - attitudeOffset.pitch)
+        faceNode.eulerAngles.y = Float(attitude.yaw - attitudeOffset.yaw)
+        faceNode.eulerAngles.z = Float(attitude.roll - attitudeOffset.roll)
+    }
+
+    private func initializeScene() {
+        scene.background.contents = UIColor.black
+
+        let geometry = ARSCNFaceGeometry(device: MTLCreateSystemDefaultDevice()!)!
+        let material = SCNMaterial()
+        material.fillMode = .lines
+        geometry.firstMaterial = material
+        faceNode.geometry = geometry
+        scene.rootNode.addChildNode(faceNode)
+
+        let camera = SCNCamera()
+        cameraNode.camera = camera
+        cameraNode.position = .init(0, 0, 1.1)
+        scene.rootNode.addChildNode(cameraNode)
+    }
+}
+
+extension ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "airpodspro").imageScale(.large)
+            if !manager.isDeviceMotionAvailable {
+                Text("Your device doesn't support headphone motion tracking.")
+            } else if let motion {
+                Group {
+                    ProgressView("roll", value: (motion.attitude.roll + 1) / 2)
+                    ProgressView("pitch", value: (motion.attitude.pitch + 1) / 2)
+                    ProgressView("yaw", value: (motion.attitude.yaw + 1) / 2)
+                }
+                .progressViewStyle(.linear)
+            } else {
+                ProgressView()
+            }
+            SceneView(scene: scene, pointOfView: cameraNode)
+        }
+        .onAppear {
+            startUpdatingMotion()
+            initializeScene()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Headphone Motion Tracking
+
+[![Swift](https://img.shields.io/badge/Swift-5.7-orange.svg)](https://www.swift.org)
+[![Swift Playgrounds](https://img.shields.io/badge/Swift%20Playgrounds-4.2-orange.svg)](https://itunes.apple.com/jp/app/swift-playgrounds/id908519492)
+![Platform](https://img.shields.io/badge/platform-ipados%20%7C%20ios-lightgrey.svg)
+[![License](https://img.shields.io/github/license/kkk669/HeadphoneMotionTracking.swiftpm.svg)](LICENSE.txt)
+
+This app is a sample app that tracks your head motion with your AirPods.
+
+## Target platforms
+
+- iPadOS 16.1 or later
+- iOS 16.1 or later
+  
+## Build requirements
+
+- Swift Playgrounds 4.2 or later (iPadOS 16.1 or later)
+
+## Get Started
+
+1. Clone this repository
+    - To clone, I recommend using [Working Copy](https://workingcopyapp.com) or [a-Shell](https://holzschu.github.io/a-Shell_iOS/) (`lg2` command).
+1. Open HeadphoneMotionTracking.swiftpm with Swift Playgrounds
+1. Wear your AirPods on your head and run
